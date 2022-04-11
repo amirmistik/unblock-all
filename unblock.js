301b03d966301aa66c624934d8e77b2f735ed674
amirmistik <57581726+amirmistik@users.noreply.github.com>
2022-04-11 19:03:16 +0500

    Add files via upload

--- unblock.js
+++ unblock.js
@@ -1,29 +1,43 @@
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
 function clickAll(buttons) {
     for (i = 0; i < buttons.length; i++) {
         buttons[i].click();
     }
 }
+
 async function unblock(timeoutMs, maxScrolls) {
     var prevScrollY;
     var scrollY = window.scrollY;
     var numScrolls = 0;
+    
+    var list = document.querySelectorAll('[aria-label="В черном списке"]');
+    clickAll(list);
+
     do {
         window.scrollTo(0,document.body.scrollHeight);
         numScrolls++;
         prevScrollY = scrollY;
         await sleep(timeoutMs);
         scrollY = window.scrollY;
+        var list = document.querySelectorAll('[aria-label="В черном списке"]');
+        clickAll(list);
+        //var actuallyBlock = confirm("Do you want to unblock all " + list.length + " accounts?");
+        //if (actuallyBlock) {
+        //}
     } while((scrollY - prevScrollY) > 0 && (typeof maxScrolls === 'undefined' || numScrolls < maxScrolls));
 
-    var unblockButtons = document.getElementsByClassName("blocked-text")
-    var actuallyBlock = confirm("Do you want to unblock all " + unblockButtons.length + " accounts?");
-    if (actuallyBlock) {
-        clickAll(unblockButtons);
-    }
+    //var confirmUnblock = confirm("continue?");
+    //if (confirmUnblock) {
+    unblock();
+    //}
+
+    //get all the buttons where aria-label is "Blocked"
+    //var unlock = document.getElementsByName("Blocked");
 }
+
 function main() {
-    unblock(500);
+    unblock(5000);
 }
